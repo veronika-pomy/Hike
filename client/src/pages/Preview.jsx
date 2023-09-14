86b46593af88985e306efe681da178b1db5e691f
Veronika Pomyateeva <pomyateevav@gmail.com>
2023-09-13 21:02:03 -0400

    adding multiple yt videos to preview and fixing api sorting parameters for https request in Preview componenet

--- client/src/pages/Preview.jsx
+++ client/src/pages/Preview.jsx
@@ -1,24 +1,45 @@
-import React, { useState } from 'react';
-
+import React, { useState, useEffect } from 'react';
 import { searchVideoYT } from '../utils/youTubeAPI'; 
 
-const backUpUrlId = 'heH-qCBCWZU';
+// TODO: build a video carousel, check tomorrow api - ran out of quota
 
-// const result = await searchVideoYT();
+const backUpVideoUrlIds = [ 'heH-qCBCWZU', 'xEqOTy8zd6I', 'RSgC-sxfrBM', 'mfX519lK4VA', 'vjRJEFOn5nc' ];
 
-// const videoId = result.items[0].id.videoId;
-// console.log(result);
+const result = await searchVideoYT();
 
 const Preview = () => {
+
+  // in case there is an issue with yt api, default to imbedding yt videos using saved ids via https
+  const [ error, setError ] = useState('');
+  // preload an id to avoid issues
+  const [ videoId, setVideoId ] = useState('vjRJEFOn5nc');
+
+  // only once on load
+  useEffect(() => {
+    if (result.error) {
+      console.log(result.error);
+      setError(result.error);
+      setVideoId(backUpVideoUrlIds[0]);
+      console.log('backup used');
+    } else {
+      // get rid of the last object in data arr due to it not being video related 
+      result.items.pop();
+      // reverse chronologically for the last video to be first
+      const resultReverseArr = result.items.reverse();  
+      const videoId = resultReverseArr[0].id.videoId;
+      setVideoId(videoId);
+      console.log('api used');
+    };
+  }, []);
+
   return (
     <>
       <div className='preview'>
         <iframe 
           width="820" 
           height="515" 
-          // if YT search using API key not possible, will default to using video id directly
-          src={`https://www.youtube.com/embed/${backUpUrlId}`}
-          // src={`https://www.youtube.com/embed/${result ? videoId : backUpUrlId}`}
+        // if YT search using API key not possible, will default to using video id directly
+          src={`https://www.youtube.com/embed/${videoId}`}
           title="YouTube video player" 
           allow="accelerometer; 
                 autoplay; 
--- client/src/utils/youTubeAPI.js
+++ client/src/utils/youTubeAPI.js
@@ -1,7 +1,7 @@
 // make request to YT API
 
 export const searchVideoYT = () => {
-    return fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=UCDLWkvdzTe9hARN3phcISmg&maxResults=1&order=date&key=${process.env.REACT_APP_YT_API_KEY}`)
+    return fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=${process.env.REACT_APP_YT_CHANNEL_ID}&maxResults=10&order=date&key=${process.env.REACT_APP_YT_API_KEY}`)
     .then((result) => {
         return result.json();
     });
