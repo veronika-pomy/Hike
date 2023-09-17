19778d3923bf7f1c3cc8c256790396495bbc7428
Veronika Pomyateeva <pomyateevav@gmail.com>
2023-09-17 13:21:22 -0400

    set up video Id variable state to handle a carosuel of videos instead of just on video in the preview componenet, set up preview css file, set up a carousel componenet

--- client/src/pages/Preview.jsx
+++ client/src/pages/Preview.jsx
@@ -1,56 +1,68 @@
 import React, { useState, useEffect } from 'react';
+import '../style/Preview.css';
 import { searchVideoYT } from '../utils/youTubeAPI'; 
 
-// TODO: build a video carousel, check tomorrow api - ran out of quota
+const backUpVideoUrlIds = [ 'WfxxyNoAxRU', 'wwm37AmD7j0', 'O8MvC9KDHWI', 'einJJ-MWweA', 'gX3KEBvNh_4' ];
 
-const backUpVideoUrlIds = [ 'heH-qCBCWZU', 'xEqOTy8zd6I', 'RSgC-sxfrBM', 'mfX519lK4VA', 'vjRJEFOn5nc' ];
+// TODO: check YT api tomorrow - ran out of quota again
 
-const result = await searchVideoYT();
+const results = await searchVideoYT();
 
 const Preview = () => {
 
   // in case there is an issue with yt api, default to imbedding yt videos using saved ids via https
   const [ error, setError ] = useState('');
   // preload an id to avoid issues
-  const [ videoId, setVideoId ] = useState('vjRJEFOn5nc');
+  const [ videoIds, setVideoIds ] = useState(backUpVideoUrlIds);
 
   // only once on load
   useEffect(() => {
-    if (result.error) {
-      console.log(result.error);
-      setError(result.error);
-      setVideoId(backUpVideoUrlIds[0]);
-      console.log('backup used');
+    if (results.error) {
+      // console.log(result.error);
+      setError(results.error);
+      setVideoIds(backUpVideoUrlIds);
+      // console.log('backup used');
     } else {
-      // get rid of the last object in data arr due to it not being video related 
-      result.items.pop();
-      // reverse chronologically for the last video to be first
-      const resultReverseArr = result.items.reverse();  
-      const videoId = resultReverseArr[0].id.videoId;
-      setVideoId(videoId);
-      console.log('api used');
+      let apiVideoDataArr = [];
+      for (let i = 0; i <= results.items.length - 1; i++) {
+        apiVideoDataArr.push(results.items[0].id.videoId);
+      };
+      setVideoIds(apiVideoDataArr);
+      // console.log('api used');
     };
   }, []);
 
   return (
     <>
       <div className='preview'>
-        <iframe 
-          width="820" 
-          height="515" 
-        // if YT search using API key not possible, will default to using video id directly
-          src={`https://www.youtube.com/embed/${videoId}`}
-          title="YouTube video player" 
-          allow="accelerometer; 
-                autoplay; 
-                clipboard-write; 
-                encrypted-media; 
-                gyroscope; 
-                picture-in-picture; 
-                web-share" 
-          allowFullScreen
-        >
-        </iframe>
+        <div className='video-carousel'>
+          <button className='carousel-btn'>
+          </button>
+          <div className='casousel-container'>
+            <ul className='carousel-list'>
+              {videoIds.map((videoId) => (
+                <li className='carousel-item'>
+                  <iframe 
+                    width="820" 
+                    height="515" 
+                    // if YT search using API key not possible, will default to using video id directly
+                    src={`https://www.youtube.com/embed/${videoId}`}
+                    title="YouTube video player" 
+                    allow="accelerometer; 
+                          autoplay; 
+                          clipboard-write; 
+                          encrypted-media; 
+                          gyroscope; 
+                          picture-in-picture; 
+                          web-share" 
+                    allowFullScreen
+                  >
+                  </iframe>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
       </div>
   </>
   )
