7fc71c8042bf4ce52c8b86e6b2efa547e29cea7c
Veronika Pomyateeva <pomyateevav@gmail.com>
2024-04-05 14:22:53 -0400

    updated scroll to top on route change

--- client/src/pages/AppContainer.jsx
+++ client/src/pages/AppContainer.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import AuthService from '../utils/auth';
+import ScrollToTop from '../utils/ScrollToTop';
 
 import '../style/AppContainer.css';
 
@@ -19,18 +20,19 @@ const AppContainer = () => {
   return (
     <>
         <Router>
-            <NavBar />
-            <Routes>
-                <Route path='/' exact element={<Home />} />
-                <Route path='/dashboard' element={AuthService.loggedIn() ? <Dashboard /> : <Navigate to='/'/>} />
-                <Route path='/about' exact element={<About />} />
-                <Route path='/services' exact element={<Services />} />
-                <Route path='/sign-up' exact element={AuthService.loggedIn() ? <Navigate to='/dashboard' /> : <SignUp />} />
-                <Route path='/sign-in' exact element={AuthService.loggedIn() ? <Navigate to='/dashboard' /> : <SignIn />} />
-                <Route path='/preview' exact element={<Preview />} />
-                <Route path='/*' exact element={<Error />} />
-            </Routes>
-            <Footer />
+          <ScrollToTop />
+          <NavBar />
+          <Routes>
+              <Route path='/' exact element={<Home />} />
+              <Route path='/dashboard' element={AuthService.loggedIn() ? <Dashboard /> : <Navigate to='/'/>} />
+              <Route path='/about' exact element={<About />} />
+              <Route path='/services' exact element={<Services />} />
+              <Route path='/sign-up' exact element={AuthService.loggedIn() ? <Navigate to='/dashboard' /> : <SignUp />} />
+              <Route path='/sign-in' exact element={AuthService.loggedIn() ? <Navigate to='/dashboard' /> : <SignIn />} />
+              <Route path='/preview' exact element={<Preview />} />
+              <Route path='/*' exact element={<Error />} />
+          </Routes>
+          <Footer />
         </Router>
     </>
   )
